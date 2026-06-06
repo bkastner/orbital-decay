4e74d115876d5b5c72495b28d2213e2c3c8d9f57
Ben Kastner <benjamin@benjaminkastner.com>
2026-06-06 15:38:29 -0600

    CelestrakClient: Allow reading from local file for testing purposes

--- src/celestrak_client.py
+++ src/celestrak_client.py
@@ -1,6 +1,7 @@
 """
 src/celestrak_client.py
 """
+import io
 import urllib3
 import logging
 from sgp4 import omm
@@ -9,28 +10,37 @@ from sgp4.api import Satrec
 logger = logging.getLogger(__name__)
 
 class CelestrakClient:
-    def __init__(self, endpoint_url="https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=csv"):
+    def __init__(self, endpoint_url="https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=csv", local_file=None):
         # Establish low-overhead HTTP connection pooling
         self.http = urllib3.PoolManager(num_pools=5, maxsize=10)
         self.endpoint_url = endpoint_url
+        self.local_file = local_file
 
     def get_active_catalog(self):
         """
         Fetches the active catalog from the network and yields parsed Satrec objects.
+        If self.local_file is present, then load data from it instead of Celestrak.  Useful to testing without hitting
+        the Celestrak API.
         Streams the response to maintain a minimal memory footprint.
         """
-        logger.info(f"Fetching OMM CSV stream from: {self.endpoint_url}")
 
-        response = self.http.request('GET', self.endpoint_url, preload_content=False)
+        if self.local_file is None:
+            logger.info(f"Fetching OMM CSV stream from: {self.endpoint_url}")
 
-        if response.status != 200:
-            raise ConnectionError(f"Celestrak HTTP Error: {response.status}")
+            response = self.http.request('GET', self.endpoint_url, preload_content=False)
 
-        text_stream = (line.decode('utf-8') for line in response)
+            if response.status != 200:
+                raise ConnectionError(f"Celestrak HTTP Error: {response.status}")
 
-        yield from self.parse_omm_stream(text_stream)
+            text_stream = (line.decode('utf-8') for line in response)
 
-        response.release_conn()
+            yield from self.parse_omm_stream(text_stream)
+
+            response.release_conn()
+        else:
+            with io.open(self.local_file, 'r') as local_data:
+                text_stream = (line for line in local_data)
+                yield from self.parse_omm_stream(text_stream)
 
     def parse_omm_stream(self, text_stream):
         """

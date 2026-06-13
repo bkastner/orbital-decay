2848b88903be3442a9a2d03e4acf5cf80841b3b5
Ben Kastner <benjamin@benjaminkastner.com>
2026-06-13 16:17:49 -0600

    Remove immediate reentries

--- src/formatter.py
+++ src/formatter.py
@@ -5,14 +5,13 @@ Format the result as mapbox-compatible json
 """
 import json
 
-def generate_geojson(with_trajectory, immediate_decays):
+def generate_geojson(with_trajectory):
     """
     Converts a list of satellite decay trajectories into a valid GeoJSON FeatureCollection.
     Structures the data specifically for Mapbox GL JS 'line-z-offset' elevated lines.
     """
     features = []
 
-    # 1. Handle the trajectory lines (LineString)
     for event in with_trajectory:
         feature = {
             "type": "Feature",
@@ -30,23 +29,6 @@ def generate_geojson(with_trajectory, immediate_decays):
         }
         features.append(feature)
 
-    # 2. Handle the immediate decays (Point)
-    for event in immediate_decays:
-        feature = {
-            "type": "Feature",
-            "properties": {
-                "catalog_id": event["catalog_id"],
-                "satellite_name": event["name"],
-                "elevation": event["altitudes"][0],  # Grab the single float
-                "timestamp": event["timestamps"][0],  # Grab the single string
-                "type": "immediate_decay"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": event["trajectory"][0]  # Flatten from [[lon, lat]] to [lon, lat]
-            }
-        }
-        features.append(feature)
 
     feature_collection = {
         "type": "FeatureCollection",

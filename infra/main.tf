261880c65a8f8def0c46f71c51adab7256d655fe
Ben Kastner <benjamin@benjaminkastner.com>
2026-07-14 18:32:36 -0600

    Terraform: import aws_s3_bucket.frontend (with prevent_destroy)

--- infra/main.tf
+++ infra/main.tf
@@ -11,3 +11,12 @@ resource "aws_ecr_repository" "orbital_decay" {
     scan_on_push = false
   }
 }
+
+resource "aws_s3_bucket" "frontend" {
+  bucket              = "orbital-decay-frontend-864144288881-us-west-2-an"
+  force_destroy       = false
+   lifecycle {
+    prevent_destroy = true
+  }
+}
+

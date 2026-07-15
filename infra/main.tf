22ea0cbfd1dfe73ddde0b9efba1b5ac5c4eed10c
Ben Kastner <benjamin@benjaminkastner.com>
2026-07-14 18:21:58 -0600

    Terraform: import aws_ecr_repository.orbital_decay

--- infra/main.tf
+++ infra/main.tf
@@ -1,2 +1,13 @@
 # Resources will be added here as we import them, then split into
 # per-domain files (storage.tf, compute.tf, etc.) as the config grows.
+
+resource "aws_ecr_repository" "orbital_decay" {
+  image_tag_mutability = "IMMUTABLE"
+  name                 = "orbital-decay"
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}

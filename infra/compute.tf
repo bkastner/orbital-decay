0ca45dde7c759b7d0e8c70f0c97a38d54c633fd4
Ben Kastner <benjamin@benjaminkastner.com>
2026-07-15 17:47:49 -0600

    Terraform: import aws_ecs_task_definition

--- infra/compute.tf
+++ infra/compute.tf
@@ -21,3 +21,57 @@ resource "aws_ecs_cluster" "orbital-decay-cluster" {
     value = "disabled"
   }
 }
+
+resource "aws_ecs_task_definition" "app" {
+  container_definitions = jsonencode([{
+    environment = [{
+      name  = "CELESTRAK_ENDPOINT_URL"
+      value = "https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=csv"
+      }, {
+      name  = "S3_BUCKET_NAME"
+      value = "orbital-decay-frontend-864144288881-us-west-2-an"
+      }, {
+      name  = "WORKER_COUNT"
+      value = "8"
+    }]
+    environmentFiles = []
+    essential        = true
+    image            = "864144288881.dkr.ecr.us-west-2.amazonaws.com/orbital-decay:v4.0"
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-create-group  = "true"
+        awslogs-group         = "/ecs/orbital-decay-task"
+        awslogs-region        = "us-west-2"
+        awslogs-stream-prefix = "ecs"
+      }
+      secretOptions = []
+    }
+    mountPoints = []
+    name        = "orbital-decay-app"
+    portMappings = [{
+      appProtocol   = "http"
+      containerPort = 80
+      hostPort      = 80
+      name          = "orbital-decay-app-80-tcp"
+      protocol      = "tcp"
+    }]
+    systemControls = []
+    ulimits        = []
+    volumesFrom    = []
+  }])
+  cpu                      = "4096"
+  enable_fault_injection   = false
+  execution_role_arn       = "arn:aws:iam::864144288881:role/ecsTaskExecutionRole"
+  family                   = "orbital-decay-task"
+  memory                   = "8192"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  skip_destroy             = true
+  task_role_arn            = "arn:aws:iam::864144288881:role/OrbitalDecayFargateTaskRole"
+  track_latest             = false
+  runtime_platform {
+    cpu_architecture        = "ARM64"
+    operating_system_family = "LINUX"
+  }
+}

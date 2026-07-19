ddccda684e19593895e2c5c9e102f8a4b1fa470d
Ben Kastner <benjamin@benjaminkastner.com>
2026-07-19 16:42:29 -0600

    Terraform: refactor common hard-coded values into variables
    Also improve formatting

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,17 +1,17 @@
 resource "aws_iam_role_policy_attachment" "task_role_s3" {
-  policy_arn = "arn:aws:iam::864144288881:policy/OrbitalDecayS3UploadPolicy"
-  role       = "OrbitalDecayFargateTaskRole"
+  policy_arn = aws_iam_policy.s3_upload.arn
+  role       = aws_iam_role.task_role.name
 }
 
 resource "aws_iam_policy" "s3_upload" {
-  description                       = "Policy to allow upload from compute container to S3 bucket"
-  name                              = "OrbitalDecayS3UploadPolicy"
-  path                              = "/"
+  description = "Policy to allow upload from compute container to S3 bucket"
+  name        = "OrbitalDecayS3UploadPolicy"
+  path        = "/"
   policy = jsonencode({
     Statement = [{
       Action   = ["s3:PutObject"]
       Effect   = "Allow"
-      Resource = "arn:aws:s3:::orbital-decay-frontend-864144288881-us-west-2-an/decays.geojson"
+      Resource = "${aws_s3_bucket.frontend.arn}/decays.geojson"
       Sid      = "AllowGeoJsonUpload"
     }]
     Version = "2012-10-17"
